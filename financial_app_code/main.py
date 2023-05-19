d596d64dabbda59f9733b58cf1d7828a371e6165
Kostya Dmitriev <dmitrievkons2000@gmail.com>
2023-05-19 19:07:57 +0300

    Add settings

--- financial_app_code/main.py
+++ financial_app_code/main.py
@@ -1,17 +1,19 @@
 from flask import Flask
 
-financial_app=Flask(__name__)
+
+financial_app = Flask(__name__)
 
 
 @financial_app.route('/')
 def index():
     return 'Hi'
 
+
 @financial_app.route('/transactions')
 def transactions():
     return 'pon'
 
+
 @financial_app.route('/categories')
 def categories():
     return 'ya lublu ponchiki'
-
---
+++ financial_app_code/settings.py
@@ -0,0 +1,14 @@
+import dotenv
+import os
+from urllib.parse import urlparse
+
+
+dotenv.load_dotenv()
+DATABASE_URL = urlparse(os.getenv('DATABASE_URL'))
+
+
+DB_HOST = DATABASE_URL.hostname
+DB_PORT = DATABASE_URL.port
+DB_NAME = DATABASE_URL.path[1:]
+DB_USER = DATABASE_URL.username
+DB_PASS = DATABASE_URL.password
